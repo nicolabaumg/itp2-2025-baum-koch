e2856c74401cb58b435ffb8ac3662169dbf62e62
koch200188 <koch200188@sr.htlweiz.at>
2025-03-17 10:56:49 +0100

    bug fix

--- backend/api/recipe.py
+++ backend/api/recipe.py
@@ -1,38 +1,37 @@
 from fastapi import APIRouter, Depends, HTTPException
-from crud.recipe import RecipeCRUD, get_recipe
-from schemas.recipe import recipeSchema
+from schemas.recipe import recipeSchema, recipeDB
 from crud.dependencies import get_recipe_crud
 from typing import List
 
 router = APIRouter()
 
-@router.post("/recipe/", response_model=recipeSchema)
-async def create_recipe_route(recipe, db: RecipeCRUD = Depends(get_recipe_crud)):
+@router.post("/recipe/", response_model=recipeDB)
+async def create_recipe_route(recipe: recipeSchema, db: recipeDB = Depends(get_recipe_crud)):
     return await db.create_recipe(recipe)
 
-@router.put("/recipe/", response_model=recipeSchema)
-async def update_recipe_route(recipe, recipe_id: int, db: RecipeCRUD = Depends(get_recipe_crud)):
+@router.put("/recipe/", response_model=recipeDB)
+async def update_recipe_route(recipe, recipe_id: int, db: recipeDB = Depends(get_recipe_crud)):
     recipe = await db.update_recipe(recipe_id, recipe)
     if recipe is None: 
         raise HTTPException(status_code=404, detail="Recipe not found")
     return recipe
 
-@router.delete("/recipe/", response_model=recipeSchema)
-async def delete_recipe_route(recipe_id: int, db: RecipeCRUD = Depends(get_recipe_crud)):
+@router.delete("/recipe/", response_model=recipeDB)
+async def delete_recipe_route(recipe_id: int, db: recipeDB = Depends(get_recipe_crud)):
     success = await db.delete_recipe(recipe_id)
     if not success:
         raise HTTPException(status_code=404, detail="Recipe not found")
     return {"message": "Recipe deleted successfully"}
 
-@router.get("/recipe/", response_model=recipeSchema)
-async def get_recipe_route(recipe_id: int, db: RecipeCRUD = Depends(get_recipe_crud)):
+@router.get("/recipe/", response_model=recipeDB)
+async def get_recipe_route(recipe_id: int, db: recipeDB = Depends(get_recipe_crud)):
     recipe = await db.get_recipe(recipe_id)
     if recipe is None:
         raise HTTPException(status_code=404, detail="Recipe not found")
     return recipe
 
-@router.get("/recipe/", response_model=List[recipeSchema])
-async def get_all_recipes_route(db: RecipeCRUD = Depends(get_recipe_crud)):
+@router.get("/recipe/", response_model=List[recipeDB])
+async def get_all_recipes_route(db: recipeDB = Depends(get_recipe_crud)):
     recipes = await db.get_all_recipes()
     if not recipes:
         raise HTTPException(status_code=404, detail="Recipes not found")
--- backend/crud/recipe.py
+++ backend/crud/recipe.py
@@ -2,7 +2,8 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from models.recipe import RecipeModels
 from models.steps import StepsModels
-from schemas.recipe import recipeSchema
+from schemas.recipe import recipeSchema, recipeDB
+from typing import List
 
 class RecipeCRUD:
     db_session = None
@@ -17,6 +18,8 @@ class RecipeCRUD:
         )
         self.db_session.add(db_recipe)
         await self.db_session.commit()
+        await self.db_session.refresh(db_recipe)
+
 
         for step in recipe.steps:
             new_step = StepsModels(recipe_id = db_recipe.id, text = step.text)
@@ -28,7 +31,7 @@ class RecipeCRUD:
     async def update_recipe(self, recipe_id, recipe: recipeSchema):
         stmt = select(RecipeModels).where(RecipeModels.id == recipe_id)
         result = await self.db_session.execute(stmt)
-        found_recipe = result.first()
+        found_recipe = result.scalars().first()
 
         if found_recipe == None:
             return None
@@ -50,7 +53,7 @@ class RecipeCRUD:
     async def delete_recipe(self, recipe_id):
         stmt = select(RecipeModels).where(RecipeModels.id == recipe_id)
         result = await self.db_session.execute(stmt)
-        found_recipe = result.first()
+        found_recipe = result.scalars().first()
 
         if found_recipe == None:
             return None
@@ -61,12 +64,12 @@ class RecipeCRUD:
     async def get_recipe(self, recipe_id, recipe: recipeSchema):
         stmt = select(RecipeModels).where(RecipeModels.id == recipe_id)
         result = await self.db_session.execute(stmt)
-        found_recipe = result.first()
+        found_recipe = result.scalars().first()
 
         return found_recipe
     
-    async def get_all_recipes(self):
+    async def get_all_recipes(self) -> List[recipeDB]:
         stmt = select(RecipeModels)
         result = await self.db_session.execute(stmt)
-        all_recipes = result.all()
+        all_recipes = result.scalars().all()
         return all_recipes
